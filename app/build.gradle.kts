e394f055a66b0e8dec35225f43e1652a5f694743
izakdvlpr <izakdvlpr@gmail.com>
2023-01-13 13:07:42 -0300

    feat: import designsystem and home page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,71 +1,74 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+  id("com.android.application")
+  id("org.jetbrains.kotlin.android")
 }
 
 android {
-    namespace = "com.izakdvlpr.composetemplate"
-    compileSdk = 32
+  namespace = "com.izakdvlpr.composetemplate"
+  compileSdk = Configurations.compileSdk
 
-    sourceSets.all {
-        kotlin.srcDir("src/${name}/kotlin")
+  sourceSets.all {
+    kotlin.srcDir("src/${name}/kotlin")
+  }
+
+  defaultConfig {
+    applicationId = "com.izakdvlpr.composetemplate"
+    minSdk = Configurations.minSdk
+    targetSdk = Configurations.targetSdk
+    versionCode = Configurations.versionCode
+    versionName = Configurations.versionName
+
+    vectorDrawables {
+      useSupportLibrary = true
     }
+  }
 
-    defaultConfig {
-        applicationId = "com.izakdvlpr.composetemplate"
-        minSdk = 21
-        targetSdk = 32
-        versionCode = 1
-        versionName = "1.0"
+  buildTypes {
+    release {
+      isMinifyEnabled = false
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-        vectorDrawables {
-            useSupportLibrary = true
-        }
+      proguardFiles(
+        getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro"
+      )
     }
+  }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
 
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
+  kotlinOptions {
+    jvmTarget = JavaVersion.VERSION_11.toString()
+  }
+
+  buildFeatures {
+    compose = true
+  }
+
+  composeOptions {
+    kotlinCompilerExtensionVersion = libs.versions.androidxCompose.get()
+  }
+
+  packagingOptions {
+    resources {
+      excludes += "/META-INF/{AL2.0,LGPL2.1}"
     }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }
-
-    buildFeatures {
-        compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.2.0"
-    }
-
-    packagingOptions {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
+  }
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.tooling)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material)
+  implementation(project(":core:designsystem"))
+
+  implementation(project(":features:home"))
+
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.lifecycle.runtime.ktx)
+  implementation(libs.androidx.activity.compose)
+
+  implementation(libs.androidx.compose.ui)
+  implementation(libs.androidx.compose.ui.tooling)
+  implementation(libs.androidx.compose.ui.tooling.preview)
+  implementation(libs.androidx.compose.material)
 }
--- app/src/main/kotlin/com/izakdvlpr/composetemplate/MainActivity.kt
+++ app/src/main/kotlin/com/izakdvlpr/composetemplate/MainActivity.kt
@@ -3,17 +3,12 @@ package com.izakdvlpr.composetemplate
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.izakdvlpr.composetemplate.ui.theme.ComposeTemplateTheme
+import com.izakdvlpr.composetemplate.core.designsystem.theme.ComposeTemplateTheme
+import com.izakdvlpr.composetemplate.features.home.ComposeTemplateHome
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,23 +20,9 @@ class MainActivity : ComponentActivity() {
           modifier = Modifier.fillMaxSize(),
           color = MaterialTheme.colors.background,
         ) {
-          HelloWorld()
+          ComposeTemplateHome()
         }
       }
-    }
-  }
-}
-
-
-@Composable
-fun HelloWorld() {
-  Column(
-    modifier = Modifier.fillMaxSize(),
-    horizontalAlignment = Alignment.CenterHorizontally,
-    verticalArrangement = Arrangement.Center
-  ) {
-    Button(onClick = {  }) {
-      Text(text = "Hello World")
     }
   }
 }
